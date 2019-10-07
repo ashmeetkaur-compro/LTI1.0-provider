9ab385d8da528d9ce9c184a7ad9b10048aaead9f
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2019-10-07 12:28:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+const oauth = require('oauth-sign');
+const app = express();
+const method = 'POST';
+const timestamp = Math.round(Date.now() / 1000);
+const config = require('E:/PracticeProjects/lti1.1_demo/config.json');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.post('/', (req,res) => {
+  var params = {
+    // LTI Required Parameters
+    lti_message_type: 'basic-lti-launch-request',
+    lti_version: 'LTI-1p0',
+    resource_link_id: 'resourceLinkId',
+    oauth_consumer_key: 'consumer_key',
+    oauth_signature_method: 'HMAC-SHA1',
+    oauth_timestamp: timestamp,
+    oauth_version: '1.0'
+};
+var action = "";
+var signature = oauth.hmacsign(method, action, params, config.secret);
+console.log("provider  Signature ->"+signature);
+
+var consumer_signature = req.body.signature;
+console.log("client  Signature ->"+req.body.signature);
+
+
+if(consumer_signature === signature) {
+  res.sendFile("E:/PracticeProjects/lti1.1_demo/launch.json");
+}
+else{
+  res.json({"message": "Signature not verified"});
+}
+});
+
+var port = (process.env.PORT || 5000);
+
+var server = app.listen(port, function () {
+  console.log("Express is working on port " + port);
+});
+
+
