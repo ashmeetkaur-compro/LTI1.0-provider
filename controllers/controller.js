b130cd441003c345fec6d7f065df3efabe8da876
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2019-10-09 12:20:54 +0530

    method added

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,15 +1,17 @@
 const oauth = require('oauth-sign');
 const config = require('../config');
+const method = 'POST';
 
 exports.statusLaunch = (req,res) => {
   res.send('Hello World!')
 };
 
 exports.verifyLaunch = (req,res) => {
-
+  
   let consumer_signature = req.body.oauth_signature;
   delete req.body.oauth_signature;
   let provider_signature = oauth.hmacsign(method, config.Provider_URL, req.body, config.secret);
+ 
 
   if(consumer_signature === provider_signature) {
     res.json({"message": "LTI Provider Launch successful"});
